f3557d4a5e54631eaf6671a5b0c8745a23cd0bc2
Josh Zimmerman <jz1179@gmail.com>
2021-04-12 14:53:02 -0400

    Added populate table rows function

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 let myLibrary = [];
 
-function Book(title, author, pages, hasRead) {
+function Book(title, author, pages, status) {
   this.title = title,
   this.author = author,
   this.pages = pages,
-  this.hasRead = hasRead
+  this.status = status
 }
 
 Book.prototype.toggleRead = function() {
-  if (this.hasRead) {
-    this.hasRead = false;
+  if (this.status === "read") {
+    this.status = "not read";
   } else {
-    this.hadRead = true;
+    this.status = "read";
   }
 }
 
@@ -21,6 +21,26 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+
 const exampleBook1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, "read");
 const exampleBook2 = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 1137, "not read");
+myLibrary.push(exampleBook1);
+myLibrary.push(exampleBook2);
 
+const docTable = document.querySelector(".book-table");
+
+function populateCollection() {
+  myLibrary.forEach((book) => {
+    const tableRow = `
+    <tr>
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+      <td>${book.status}</td>
+      <td><button class="button-primary">delete</button></td>
+    </tr>
+    `;
+    docTable.insertAdjacentHTML("afterbegin", tableRow);
+  });
+}
+populateCollection();
