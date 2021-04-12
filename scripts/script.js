7a52f7541e0ab0610d5ecc1932351b5b4e2dbc0e
Josh Zimmerman <jz1179@gmail.com>
2021-04-12 12:47:14 -0400

    Book constructor added

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,25 @@
-//Example Object Constructor from TOP-JavaScript-Objects and Objects Constructors
+let myLibrary = [];
+
 function Book(title, author, pages, hasRead) {
-  this.title = title
-  this.author = author
-  this.pages = pages
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
   this.hasRead = hasRead
-  this.info = function() {
-    let readText = ''
-    if (hasRead === true) {
-      readText = 'finished reading'
-    } else {
-      readText = 'not read yet'
-    }
-    return `${title}, by ${author}, ${pages} pages, ${readText}`
+}
+
+Book.prototype.toggleRead = function() {
+  if (this.hasRead) {
+    this.hasRead = false;
+  } else {
+    this.hadRead = true;
   }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-console.log(theHobbit.info());
+function addBookToLibrary(book) {
+  //Takes user input,creates a new book object and adds it to library array
+  
+  myLibrary.push(book);
+}
 
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+console.log(theHobbit);
