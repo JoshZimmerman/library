4bddbc9b17d39364b3c52a2c06b54d73128b8dc7
Josh Zimmerman <jz1179@gmail.com>
2021-04-13 11:48:21 -0400

    Added localStorage read and write functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,13 @@
 let myLibrary = [];
 
-function Book(title, author, pages, status) {
-  this.title = title,
-  this.author = author,
-  this.pages = pages,
-  this.status = status
-}
-
-Book.prototype.toggleRead = function() {
-  if (this.status === "read") {
-    this.status = "not read";
-  } else {
-    this.status = "read";
+class Book {
+  constructor(title, author, pages, status) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.status = status
   }
 }
-
-//Set up default
-const exampleBook1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, "read");
-const exampleBook2 = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 1137, "not read");
-myLibrary.push(exampleBook1);
-myLibrary.push(exampleBook2);
 
 //DOM Objects
 const docTitle = document.getElementById("titleInput");
@@ -30,6 +18,25 @@ const docTable = document.querySelector(".book-table");
 const docForm = document.querySelector("form");
 
 
+function checkLocalStorage() {
+  if(localStorage.getItem('myLibrary')) {
+    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+  } else {
+    setExampleBooks();
+  }
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
+
+function setExampleBooks() {
+  const exampleBook1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, "read");
+  const exampleBook2 = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 1137, "not read");
+  myLibrary.push(exampleBook1);
+  myLibrary.push(exampleBook2);
+}
+
 function addBookToLibrary() {
   //Takes user input,creates a new book object and adds it to library array
   //add form validation
@@ -37,7 +44,15 @@ function addBookToLibrary() {
   myLibrary.push(newBook);
 }
 
-function populateCollection() {
+function toggleRead(book) {
+  if (book.status === "read") {
+    book.status = "not read";
+  } else {
+    book.status = "read";
+  }
+}
+
+function populatePageTable() {
   docTable.innerHTML = "";
   myLibrary.forEach((book, index) => {
     const tableRow = `
@@ -57,17 +72,18 @@ function populateCollection() {
 }
 
 function resetForm() {
-  document.querySelector("#titleInput").value = "";
-  document.querySelector("#authorInput").value = "";
-  document.querySelector("#pagesInput").value = "";
+  docTitle.value = "";
+  docAuthor.value = "";
+  docPages.value = "";
 }
 
 function setupToggles() {
   let toggleButtons = document.querySelectorAll(".button-toggle");
   toggleButtons.forEach(btn => {
     btn.addEventListener('click', (e) => {
-      myLibrary[e.target.dataset.index].toggleRead();
-      populateCollection();
+      toggleRead(myLibrary[e.target.dataset.index]);
+      updateLocalStorage();
+      populatePageTable();
     });
   });
 }
@@ -77,21 +93,27 @@ function setupDeletes() {
   delButtons.forEach(btn => {
     btn.addEventListener('click', (e) => {
       myLibrary.splice(e.target.dataset.indexNumber, 1);
-      populateCollection();
+      updateLocalStorage();
+      populatePageTable();
     });
   });
 }
 
+function runOnLoad() {
+  checkLocalStorage();
+  populatePageTable();
+}
 
 //Testing
-populateCollection();
+runOnLoad();
 
 
 //Set up Event Listeners
 docForm.addEventListener("submit", function(e) {
   e.preventDefault();
   addBookToLibrary();
-  populateCollection();
+  updateLocalStorage();
+  populatePageTable();
   resetForm();
 });
 
